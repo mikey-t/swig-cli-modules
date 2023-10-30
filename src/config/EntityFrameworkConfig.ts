d71c1933587ab4f47cd4737cabdfdcef58495b23
Mike Thompson <mikeylikes23@gmail.com>
2023-10-30 10:31:56 -0600

    Improved EF module error messages.

--- src/config/EntityFrameworkConfig.ts
+++ src/config/EntityFrameworkConfig.ts
@@ -17,13 +17,13 @@ export class EntityFrameworkConfig {
 
   throwIfInvalid() {
     if (!this.dbMigratorPath) {
-      throw new Error('SwigEntityFrameworkConfig.dbMigratorPath is required')
+      throw new Error('SwigEntityFrameworkConfig error - dbMigratorPath is required')
     }
     if (!fs.existsSync(this.dbMigratorPath)) {
-      throw new Error('SwigEntityFrameworkConfig.dbMigratorPath error - path does not exist')
+      throw new Error(`SwigEntityFrameworkConfig error - dbMigratorPath path does not exist: ${this.dbMigratorPath}`)
     }
     if (this.dbContexts.length === 0) {
-      throw new Error('SwigEntityFrameworkConfig.dbContexts must have at least one entry')
+      throw new Error('SwigEntityFrameworkConfig error - dbContexts must have at least one entry')
     }
   }
 }
